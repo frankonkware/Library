f03e624d2781c9b87c9c29a542a0d6ba4cff4df7
frankonkware <frankonkware@gmail.com>
2020-04-28 09:00:39 +0300

    Added godreads API

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -9,48 +9,56 @@ const books = [
     title: 'War and Peace',
     genre: 'Historical Fiction',
     author: 'Lev Nikolayevich Tolstoy',
+    bookId: 656,
     read: false
   },
   {
     title: 'Les Misérables',
     genre: 'Historical Fiction',
     author: 'Victor Hugo',
+    bookId: 24280,
     read: false
   },
   {
     title: 'The Time Machine',
     genre: 'Science Fiction',
     author: 'H. G. Wells',
+    bookId: 2493,
     read: false
   },
   {
     title: 'A Journey into the Center of the Earth',
     genre: 'Science Fiction',
     author: 'Jules Verne',
+    bookId: 35262749,
     read: false
   },
   {
     title: 'The Dark World',
     genre: 'Fantasy',
     author: 'Henry Kuttner',
+    bookId: 18513759,
     read: false
   },
   {
     title: 'The Wind in the Willows',
     genre: 'Fantasy',
     author: 'Kenneth Grahame',
+    bookId: 5659,
     read: false
   },
   {
     title: 'Life On The Mississippi',
     genre: 'History',
     author: 'Mark Twain',
+    bookId: 99152,
     read: false
   },
   {
     title: 'Childhood',
     genre: 'Biography',
     author: 'Lev Nikolayevich Tolstoy',
+    bookId: 414999,
     read: false
   }];
 
--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,8 +1,29 @@
 /* eslint-disable linebreak-style */
+const axios = require('axios');
+
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
+
+const parser = xml2js.Parser({explicitArray: false });
+
 function goodreadsService() {
-  function getBookById() {
+  function getBookById(id) {
     return new Promise((resolve, reject) => {
-      resolve({ description: 'Our Description' });
+      axios.get(`https://www.goodreads.com/book/show/${id}.xml?key=XSzLl2Db0bz4hxr4bOAXGg`)
+        .then((response) => {
+          parser.parseString(response.data, (err, result) => {
+            if (err) {
+              debug(err);
+            } else {
+              debug(result);
+              resolve(result.GoodreadsResponse.book);
+            }
+          });
+        })
+        .catch((error) => {
+          reject(error);
+          debug(error);
+        });
     });
   }
   return { getBookById };
